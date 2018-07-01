518d6cc5e130de04548da2c6957cedb4af4bb323
Sebastian Krajewski <mail@wieku.me>
2018-07-01 16:01:02 +0200

    Sort objects after slider conversion

--- dance/schedulers/sliderprocessor.go
+++ dance/schedulers/sliderprocessor.go
@@ -1,6 +1,9 @@
 package schedulers
 
-import "github.com/wieku/danser/beatmap/objects"
+import (
+	"github.com/wieku/danser/beatmap/objects"
+	"sort"
+)
 
 func objectPreProcess(hitobject objects.BaseObject, sliderDance bool) ([]objects.BaseObject, bool) {
 	if s1, ok1 := hitobject.(*objects.Slider); ok1 && sliderDance {
@@ -12,7 +15,9 @@ func objectPreProcess(hitobject objects.BaseObject, sliderDance bool) ([]objects
 func PreprocessQueue(index int, queue []objects.BaseObject, sliderDance bool) []objects.BaseObject {
 	if arr, ok := objectPreProcess(queue[index], sliderDance); ok {
 		if index < len(queue) -1 {
-			return append(queue[:index], append(arr, queue[index+1:]...)...)
+			queue1 := append(queue[:index], append(arr, queue[index+1:]...)...)
+			sort.Slice(queue1, func(i, j int) bool {return queue1[i].GetBasicData().StartTime < queue1[j].GetBasicData().StartTime})
+			return queue1
 		} else {
 			return append(queue[:index], arr...)
 		}
