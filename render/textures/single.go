6419583fd40057295c34d5d5f5f7ff7e1fa0d531
Sebastian Krajewski <mail@wieku.me>
2018-09-27 04:09:41 +0200

    Added single texture

---
+++ render/textures/single.go
@@ -0,0 +1,74 @@
+package textures
+
+import (
+	"image"
+	"github.com/go-gl/gl/v3.3-core/gl"
+	"runtime"
+	"github.com/faiface/mainthread"
+)
+
+type TextureSingle struct {
+	store     *textureStore
+	defRegion TextureRegion
+}
+
+func NewTextureSingle(width, height, mipmaps int) *TextureSingle {
+	texture := new(TextureSingle)
+	texture.store = newStore(1, width, height, mipmaps)
+	texture.defRegion = TextureRegion{0, 1, 0, 1, int32(width), int32(height), 0}
+
+	runtime.SetFinalizer(texture, texture.Dispose)
+
+	return texture
+}
+
+func LoadTextureSingle(img *image.NRGBA, mipmaps int) *TextureSingle {
+	texture := NewTextureSingle(img.Bounds().Dx(), img.Bounds().Dy(), mipmaps)
+	texture.SetData(0, 0, img.Bounds().Dx(), img.Bounds().Dy(), img.Pix)
+	return texture
+}
+
+func (texture *TextureSingle) SetData(x, y, width, height int, data []uint8) {
+	if len(data) != width*height*4 {
+		panic("Wrong number of pixels given!")
+	}
+
+	gl.TexSubImage3D(gl.TEXTURE_2D_ARRAY, 0, int32(x), int32(y), 0, int32(width), int32(height), 1, gl.RGBA, gl.UNSIGNED_BYTE, gl.Ptr(data))
+	if texture.store.mipmaps > 1 {
+		gl.GenerateMipmap(gl.TEXTURE_2D_ARRAY)
+	}
+}
+
+func (texture *TextureSingle) GetID() uint32 {
+	return texture.store.id
+}
+
+func (texture *TextureSingle) GetWidth() int32 {
+	return texture.store.width
+}
+
+func (texture *TextureSingle) GetHeight() int32 {
+	return texture.store.height
+}
+
+func (texture *TextureSingle) GetRegion() TextureRegion {
+	return texture.defRegion
+}
+
+func (texture *TextureSingle) GetLayers() int32 {
+	return 1
+}
+
+func (texture *TextureSingle) SetFiltering(min, mag Filter) {
+	texture.store.SetFiltering(min, mag)
+}
+
+func (texture *TextureSingle) Bind(loc uint) {
+	texture.store.Bind(loc)
+}
+
+func (texture *TextureSingle) Dispose() {
+	mainthread.CallNonBlock(func() {
+		texture.store.Dispose()
+	})
+}
---
+++ render/textures/texture.go
@@ -0,0 +1,78 @@
+package textures
+
+import (
+	"github.com/go-gl/gl/v3.3-core/gl"
+)
+
+type Filter int32
+
+var Filtering = struct {
+	Nearest,
+	Linear,
+	MipMap,
+	MipMapNearestNearest,
+	MipMapLinearNearest,
+	MipMapNearestLinear,
+	MipMapLinearLinear Filter
+}{gl.NEAREST, gl.LINEAR, gl.LINEAR_MIPMAP_LINEAR, gl.NEAREST_MIPMAP_NEAREST, gl.LINEAR_MIPMAP_NEAREST, gl.NEAREST_MIPMAP_LINEAR, gl.LINEAR_MIPMAP_LINEAR}
+
+type Texture interface {
+	GetID() uint32
+	GetWidth() int32
+	GetHeight() int32
+	GetRegion() TextureRegion
+	GetLayers() int32
+	SetFiltering(min, mag Filter)
+	Bind(loc uint)
+	Dispose()
+}
+
+type TextureRegion struct {
+	U1, U2, V1, V2 float32
+	Width, Height  int32
+	Layer          int32
+}
+
+type textureStore struct {
+	id                             uint32
+	layers, width, height, mipmaps int32
+}
+
+func newStore(layerNum, width, height, mipmaps int) *textureStore {
+	store := new(textureStore)
+	gl.GenTextures(1, &store.id)
+
+	store.layers = int32(layerNum)
+	store.width = int32(width)
+	store.height = int32(height)
+
+	if mipmaps < 1 {
+		mipmaps = 1
+	}
+	store.mipmaps = int32(mipmaps)
+
+	store.Bind(0)
+	gl.TexStorage3D(gl.TEXTURE_2D_ARRAY, store.mipmaps, gl.RGBA8, store.width, store.height, store.layers)
+
+	if mipmaps > 1 {
+		store.SetFiltering(Filtering.MipMap, Filtering.Linear)
+	} else {
+		store.SetFiltering(Filtering.Linear, Filtering.Linear)
+	}
+
+	return store
+}
+
+func (store *textureStore) Bind(loc uint) {
+	gl.ActiveTexture(gl.TEXTURE0 + uint32(loc))
+	gl.BindTexture(gl.TEXTURE_2D_ARRAY, store.id)
+}
+
+func (store *textureStore) SetFiltering(min, mag Filter) {
+	gl.TexParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, int32(min))
+	gl.TexParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, int32(mag))
+}
+
+func (store *textureStore) Dispose() {
+	gl.DeleteTextures(1, &store.id)
+}
