58ba56df8995b8a74b6db159dfeb66f5e0f6dd74
Sebastian Krajewski <mail@wieku.me>
2019-11-19 02:07:13 +0100

    Fix last commit

--- bmath/curves/sliderAlgo.go
+++ bmath/curves/sliderAlgo.go
@@ -1,28 +1,30 @@
-package sliders
+package curves
 
 import (
 	m2 "github.com/wieku/danser-go/bmath"
-	"github.com/wieku/danser-go/bmath/curves"
 	"github.com/wieku/danser-go/bmath"
 )
 
-type SliderAlgo struct {
-	curves   []curves.Curve
+type MultiCurve struct {
+	curves   []Curve
 	sections []float64
 	length   float64
 	scale    float64
 }
 
-func NewSliderAlgo(typ string, points []m2.Vector2d, desiredLength float64) SliderAlgo {
-	var curveList []curves.Curve
+func NewMultiCurve(typ string, points []m2.Vector2d, desiredLength float64, customTiming []float64) MultiCurve {
+	var curveList []Curve
 
 	length := 0.0
 	if len(points) < 3 {
 		typ = "L"
 	}
+
+	index := 0
+
 	switch typ {
 	case "P":
-		c := curves.NewCirArc(points[0], points[1], points[2])
+		c := NewCirArc(points[0], points[1], points[2])
 		if !c.Unstable {
 			curveList = append(curveList, c)
 			length += c.GetLength()
@@ -31,9 +33,15 @@ func NewSliderAlgo(typ string, points []m2.Vector2d, desiredLength float64) Slid
 		fallthrough
 	case "L":
 		for i := 1; i < len(points); i++ {
-			c := curves.NewLinear(points[i-1], points[i])
+			c := NewLinear(points[i-1], points[i])
 			curveList = append(curveList, c)
-			length += c.GetLength()
+			if customTiming == nil {
+				length += c.GetLength()
+			} else {
+				length += customTiming[index]
+			}
+
+			index++
 		}
 		break
 	case "B":
@@ -41,19 +49,38 @@ func NewSliderAlgo(typ string, points []m2.Vector2d, desiredLength float64) Slid
 		for i, p := range points {
 			if (i == len(points)-1 && p != points[i-1]) || (i < len(points)-1 && points[i+1] == p) {
 				pts := points[lastIndex : i+1]
-				var c curves.Curve
+				var c Curve
 				if len(pts) > 2 {
-					c = curves.NewBezier(pts)
+					c = NewBezier(pts)
 				} else if len(pts) == 1 {
-					c = curves.NewLinear(pts[0], pts[0])
+					c = NewLinear(pts[0], pts[0])
 				} else {
-					c = curves.NewLinear(pts[0], pts[1])
+					c = NewLinear(pts[0], pts[1])
 				}
 
 				curveList = append(curveList, c)
-				length += c.GetLength()
+				if customTiming == nil {
+					length += c.GetLength()
+				} else {
+					length += customTiming[index]
+				}
+
+				index++
 				lastIndex = i + 1
 			}
+		}
+		break
+	case "CB":
+		for i := 0; i < len(points)-3; i+=3 {
+			c := NewBezier(points[i : i+4])
+			curveList = append(curveList, c)
+			if customTiming == nil {
+				length += c.GetLength()
+			} else {
+				length += customTiming[index]
+			}
+
+			index++
 		}
 		break
 	case "C":
@@ -67,24 +94,32 @@ func NewSliderAlgo(typ string, points []m2.Vector2d, desiredLength float64) Slid
 		}
 
 		for i := 0; i < len(points)-3; i++ {
-			c := curves.NewCatmull(points[i : i+4])
+			c := NewCatmull(points[i : i+4])
 			curveList = append(curveList, c)
-			length += c.GetLength()
+			if customTiming == nil {
+				length += c.GetLength()
+			} else {
+				length += customTiming[index]
+			}
+
+			index++
 		}
 		break
 	}
 
 	scale := -1.0
 
-	if length > desiredLength {
-		scale = desiredLength / length
-	} else if desiredLength > length {
-		last := curveList[len(curveList)-1]
-		p2 := last.PointAt(1)
-		p3 := bmath.NewVec2dRad(last.GetEndAngle(), desiredLength-length).Add(p2)
-		c := curves.NewLinear(p2, p3)
-		curveList = append(curveList, c)
-		length += c.GetLength()
+	if desiredLength >= 0 {
+		if length > desiredLength {
+			scale = desiredLength / length
+		} else if desiredLength > length {
+			last := curveList[len(curveList)-1]
+			p2 := last.PointAt(1)
+			p3 := bmath.NewVec2dRad(last.GetEndAngle(), desiredLength-length).Add(p2)
+			c := NewLinear(p2, p3)
+			curveList = append(curveList, c)
+			length += c.GetLength()
+		}
 	}
 
 	sections := make([]float64, len(curveList)+1)
@@ -92,15 +127,19 @@ func NewSliderAlgo(typ string, points []m2.Vector2d, desiredLength float64) Slid
 	prev := 0.0
 	if len(curveList) > 1 {
 		for i := 0; i < len(curveList); i++ {
-			prev += curveList[i].GetLength() / length
+			if customTiming == nil {
+				prev += curveList[i].GetLength() / length
+			} else {
+				prev += customTiming[i] / length
+			}
 			sections[i+1] = prev
 		}
 	}
 
-	return SliderAlgo{curveList, sections, length, scale}
+	return MultiCurve{curveList, sections, length, scale}
 }
 
-func (sa *SliderAlgo) PointAt(t float64) m2.Vector2d {
+func (sa *MultiCurve) PointAt(t float64) m2.Vector2d {
 	if sa.scale > -0.5 {
 		t *= sa.scale
 	}
@@ -119,9 +158,36 @@ func (sa *SliderAlgo) PointAt(t float64) m2.Vector2d {
 	return m2.NewVec2d(512/2, 384/2)
 }
 
-func (sa *SliderAlgo) GetLength() float64 {
+func (sa *MultiCurve) NPointAt(t float64) m2.Vector2d {
+	if sa.scale > -0.5 {
+		t *= sa.scale
+	}
+	if len(sa.curves) == 1 {
+		return sa.curves[0].NPointAt(t)
+	} else {
+		t = sa.sections[len(sa.sections)-1] * t
+		for i := 1; i < len(sa.sections); i++ {
+			if t <= sa.sections[i] || i == len(sa.sections)-1 {
+				prc := (t - sa.sections[i-1]) / (sa.sections[i] - sa.sections[i-1])
+				return sa.curves[i-1].NPointAt(prc)
+			}
+		}
+	}
+
+	return m2.NewVec2d(512/2, 384/2)
+}
+
+func (sa *MultiCurve) GetLength() float64 {
 	if sa.scale > -0.5 {
 		return sa.length * sa.scale
 	}
 	return sa.length
+}
+
+func (sa *MultiCurve) GetStartAngle() float64 {
+	return sa.curves[0].GetStartAngle()
+}
+
+func (sa *MultiCurve) GetEndAngle() float64 {
+	return sa.curves[len(sa.curves)-1].GetEndAngle()
 }
