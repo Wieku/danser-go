99ea85a2279b047e13f2c31fe39d156f92059e6c
Sebastian Krajewski <mail@wieku.me>
2020-07-14 01:06:16 +0200

    Add multisampled framebuffer

--- render/framebuffer/framebuffer.go
+++ render/framebuffer/framebuffer.go
@@ -1,6 +1,7 @@
 package framebuffer
 
 import (
+	"github.com/wieku/danser-go/settings"
 	"runtime"
 
 	"github.com/faiface/mainthread"
@@ -12,9 +13,12 @@ var bindHistory []int32
 
 // Framebuffer is a fixed resolution texture that you can draw on.
 type Framebuffer struct {
-	obj  uint32
-	last int32
-	tex  *texture.TextureSingle
+	obj           uint32
+	last          int32
+	tex           *texture.TextureSingle
+	multisampled  bool
+	helperObj     uint32
+	helperTexture uint32
 }
 
 // NewFrame creates a new fully transparent Framebuffer with given dimensions in pixels.
@@ -44,6 +48,50 @@ func NewFrame(width, height int, smooth, depth bool) *Framebuffer {
 	return f
 }
 
+func NewFrameMultisample(width, height int, smooth, depth bool) *Framebuffer {
+	f := new(Framebuffer)
+
+	f.tex = texture.NewTextureSingle(width, height, 0)
+
+	gl.GenFramebuffers(1, &f.helperObj)
+
+	gl.GetIntegerv(gl.FRAMEBUFFER_BINDING, &f.last)
+	bindHistory = append(bindHistory, f.last)
+	gl.BindFramebuffer(gl.FRAMEBUFFER, f.helperObj)
+
+	f.tex.Bind(0)
+	gl.FramebufferTextureLayerARB(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, f.tex.GetID(), 0, 0)
+
+	lst := bindHistory[len(bindHistory)-1]
+	bindHistory = bindHistory[:len(bindHistory)-1]
+	gl.BindFramebuffer(gl.FRAMEBUFFER, uint32(lst))
+
+	gl.GenFramebuffers(1, &f.obj)
+
+	f.Begin()
+
+	gl.GenTextures(1, &f.helperTexture)
+	gl.BindTexture(gl.TEXTURE_2D_MULTISAMPLE, f.helperTexture)
+
+	gl.TexImage2DMultisample(gl.TEXTURE_2D_MULTISAMPLE, settings.Graphics.MSAA, gl.RGBA8, int32(width), int32(height), true)
+	gl.FramebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D_MULTISAMPLE, f.helperTexture, 0)
+
+	if depth {
+		var depthRenderBuffer uint32
+		gl.GenRenderbuffers(1, &depthRenderBuffer)
+		gl.BindRenderbuffer(gl.RENDERBUFFER, depthRenderBuffer)
+		gl.RenderbufferStorageMultisample(gl.RENDERBUFFER, settings.Graphics.MSAA, gl.DEPTH_COMPONENT, int32(width), int32(height))
+		gl.FramebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, depthRenderBuffer)
+	}
+
+	f.End()
+
+	runtime.SetFinalizer(f, (*Framebuffer).delete)
+
+	f.multisampled = true
+	return f
+}
+
 func (f *Framebuffer) delete() {
 	mainthread.CallNonBlock(func() {
 		gl.DeleteFramebuffers(1, &f.obj)
@@ -64,7 +112,18 @@ func (f *Framebuffer) Begin() {
 
 // End unbinds the Framebuffer. All draw operations will go to whatever was bound before this Framebuffer.
 func (f *Framebuffer) End() {
-	//log.Println(uint32(f.last))
+	if f.multisampled {
+		gl.BindFramebuffer(gl.READ_FRAMEBUFFER, f.obj)
+		gl.ReadBuffer(gl.COLOR_ATTACHMENT0)
+		gl.BindFramebuffer(gl.DRAW_FRAMEBUFFER, f.helperObj)
+		gl.DrawBuffer(gl.COLOR_ATTACHMENT0)
+
+		gl.BlitFramebuffer(0, 0, f.tex.GetWidth(), f.tex.GetHeight(), 0, 0, f.tex.GetWidth(), f.tex.GetHeight(), gl.COLOR_BUFFER_BIT, gl.LINEAR)
+
+		gl.BindFramebuffer(gl.READ_FRAMEBUFFER, 0)
+		gl.BindFramebuffer(gl.DRAW_FRAMEBUFFER, 0)
+	}
+
 	lst := bindHistory[len(bindHistory)-1]
 	bindHistory = bindHistory[:len(bindHistory)-1]
 	gl.BindFramebuffer(gl.FRAMEBUFFER, uint32(lst))
