805b5c89a28a70f089a781c30a940da550e721bb
Sebastian Krajewski <mail@wieku.me>
2020-07-24 14:31:57 +0200

    Change math2 to bmath

--- bmath/curves/linear.go
+++ bmath/curves/linear.go
@@ -1,19 +1,19 @@
 package curves
 
 import (
-	math2 "github.com/wieku/danser-go/bmath"
+	"github.com/wieku/danser-go/bmath"
 	"math"
 )
 
 type Linear struct {
-	Point1, Point2 math2.Vector2d
+	Point1, Point2 bmath.Vector2d
 }
 
-func NewLinear(pt1, pt2 math2.Vector2d) Linear {
+func NewLinear(pt1, pt2 bmath.Vector2d) Linear {
 	return Linear{pt1, pt2}
 }
 
-func (ln Linear) PointAt(t float64) math2.Vector2d {
+func (ln Linear) PointAt(t float64) bmath.Vector2d {
 	return ln.Point2.Sub(ln.Point1).Scl(t).Add(ln.Point1)
 }
 
