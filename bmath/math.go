8cf5c383173b2ce0594601be13586a2a95f8f9eb
Sebastian Krajewski <mail@wieku.me>
2020-07-26 04:33:51 +0200

    Add clamp functions

--- bmath/math.go
+++ bmath/math.go
@@ -18,3 +18,47 @@ func AngleBetween32(centre, p1, p2 Vector2f) float32 {
 	c := p1.Dst(p2)
 	return math32.Acos((a*a + b*b - c*c) / (2 * a * b))
 }
+
+func ClampF32(x, min, max float32) float32 {
+	return math32.Min(max, math32.Max(min, x))
+}
+
+func ClampF64(x, min, max float64) float64 {
+	return math.Min(max, math.Max(min, x))
+}
+
+func MinI(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func MaxI(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func ClampI(x, min, max int) int {
+	return MinI(max, MaxI(min, x))
+}
+
+func MinI64(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func MaxI64(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func ClampI64(x, min, max int64) int64 {
+	return MinI64(max, MaxI64(min, x))
+}
