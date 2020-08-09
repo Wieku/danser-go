693cdba73f1d7bd6153cc71aa770208939dff288
Sebastian Krajewski <mail@wieku.me>
2020-08-09 07:37:33 +0200

    Fix some sprites not being initialized correctly

--- storyboard/transformations.go
+++ storyboard/transformations.go
@@ -1,8 +1,8 @@
 package storyboard
 
 import (
-	"sort"
 	"math"
+	"sort"
 )
 
 type Transformations struct {
@@ -23,7 +23,7 @@ func (trans *Transformations) Add(command *Command) {
 		exists := false
 
 		for _, e := range trans.queue {
-			if e.command == command.command && e.start < command.start {
+			if e.command == command.command && e.start <= command.start {
 				exists = true
 				break
 			}
@@ -56,6 +56,8 @@ func (trans *Transformations) Finalize() {
 	sort.Slice(trans.commands, func(i, j int) bool {
 		return trans.commands[i].start < trans.commands[j].start
 	})
+
+	trans.processed = make([]*Command, 0, len(trans.queue))
 }
 
 func (trans *Transformations) Update(time int64) {
@@ -63,16 +65,19 @@ func (trans *Transformations) Update(time int64) {
 	if time < trans.lastTime {
 		trans.queue = make([]*Command, len(trans.commands))
 		copy(trans.queue, trans.commands)
-		trans.processed = make([]*Command, 0)
+		trans.processed = make([]*Command, 0, len(trans.queue))
 	}
 
 	for i := 0; i < len(trans.queue); i++ {
 		c := trans.queue[i]
-		if c.start <= time {
-			trans.processed = append(trans.processed, c)
-			trans.queue = append(trans.queue[:i], trans.queue[i+1:]...)
-			i--
+		if time < c.start {
+			break
 		}
+
+		trans.processed = append(trans.processed, c)
+		copy(trans.queue[i:], trans.queue[i+1:])
+		trans.queue = trans.queue[:len(trans.queue)-1]
+		i--
 	}
 
 	for i := 0; i < len(trans.processed); i++ {
@@ -82,8 +87,9 @@ func (trans *Transformations) Update(time int64) {
 			c.Apply(trans.object)
 		}
 
-		if time > c.end {
-			trans.processed = append(trans.processed[:i], trans.processed[i+1:]...)
+		if time >= c.end {
+			copy(trans.processed[i:], trans.processed[i+1:])
+			trans.processed = trans.processed[:len(trans.processed)-1]
 			i--
 		}
 	}
