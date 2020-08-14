35fa0bfe22284634c8d418e591f02ddd638c8b70
Sebastian Krajewski <mail@wieku.me>
2020-08-14 04:38:47 +0200

    Add some configs to BASS that may reduce latency

--- audio/system.go
+++ audio/system.go
@@ -13,6 +13,19 @@ import (
 )
 
 func Init() {
+
+	// Output data regardless if audio is playing
+	C.BASS_SetConfig(C.BASS_CONFIG_DEV_NONSTOP, C.DWORD(1))
+
+	// Worse time resolution but lower latency with DirectSound
+	C.BASS_SetConfig(C.BASS_CONFIG_VISTA_TRUEPOS, C.DWORD(0))
+
+	// Smaller buffer length, reduces latency
+	C.BASS_SetConfig(C.BASS_CONFIG_DEV_BUFFER, C.DWORD(10))
+
+	// Update BASS more frequently
+	C.BASS_SetConfig(C.BASS_CONFIG_DEV_PERIOD, C.DWORD(5))
+
 	if C.BASS_Init(C.int(-1), C.DWORD(44100), C.DWORD(0), nil, nil) != 0 {
 		log.Println("BASS Initialized!")
 	} else {
