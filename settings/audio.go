5c678d3514b9d5d18e092551b601bedc727e0d10
Sebastian Krajewski <mail@wieku.me>
2020-08-15 04:15:18 +0200

    Add "HitsoundPositionMultiplier" option

--- settings/audio.go
+++ settings/audio.go
@@ -4,20 +4,22 @@ var Audio = initAudio()
 
 func initAudio() *audio {
 	return &audio{
-		GeneralVolume:             0.5,
-		MusicVolume:               0.5,
-		SampleVolume:              0.5,
-		Offset:                    0,
-		IgnoreBeatmapSamples:      false,
-		IgnoreBeatmapSampleVolume: false,
+		GeneralVolume:              0.5,
+		MusicVolume:                0.5,
+		SampleVolume:               0.5,
+		Offset:                     0,
+		HitsoundPositionMultiplier: 1.0,
+		IgnoreBeatmapSamples:       false,
+		IgnoreBeatmapSampleVolume:  false,
 	}
 }
 
 type audio struct {
-	GeneralVolume             float64 //0.5
-	MusicVolume               float64 //=0.5
-	SampleVolume              float64 //=0.5
-	Offset                    int64
-	IgnoreBeatmapSamples      bool //= false
-	IgnoreBeatmapSampleVolume bool //= false
+	GeneralVolume              float64 //0.5
+	MusicVolume                float64 //=0.5
+	SampleVolume               float64 //=0.5
+	Offset                     int64
+	HitsoundPositionMultiplier float64
+	IgnoreBeatmapSamples       bool //= false
+	IgnoreBeatmapSampleVolume  bool //= false
 }
