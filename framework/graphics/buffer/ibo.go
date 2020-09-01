f48a6c13f85a761b2b328fcaa07db9a3685bda21
Sebastian Krajewski <mail@wieku.me>
2020-09-01 21:21:55 +0200

    Add sanity checks to IBO

--- framework/graphics/buffer/ibo.go
+++ framework/graphics/buffer/ibo.go
@@ -1,27 +1,32 @@
 package buffer
 
 import (
+	"fmt"
+	"github.com/faiface/mainthread"
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/wieku/danser-go/framework/graphics/history"
 	"github.com/wieku/danser-go/framework/statistic"
-	"log"
+	"runtime"
 )
 
 type IndexBufferObject struct {
 	handle   uint32
 	capacity int
+	bound    bool
+	disposed bool
 }
 
 func NewIndexBufferObject(maxIndices int) *IndexBufferObject {
-	ibo := &IndexBufferObject{}
+	ibo := new(IndexBufferObject)
 	ibo.capacity = maxIndices
-	log.Println(&ibo.handle)
+
 	gl.GenBuffers(1, &ibo.handle)
 
 	ibo.Bind()
-	emptyData := make([]uint16, maxIndices)
-	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, len(emptyData)*2, gl.Ptr(emptyData), gl.DYNAMIC_DRAW)
+	gl.BufferData(gl.ELEMENT_ARRAY_BUFFER, maxIndices*2, gl.Ptr(nil), gl.DYNAMIC_DRAW)
 	ibo.Unbind()
+
+	runtime.SetFinalizer(ibo, ibo.Dispose)
 
 	return ibo
 }
@@ -31,31 +36,21 @@ func (ibo *IndexBufferObject) Capacity() int {
 }
 
 func (ibo *IndexBufferObject) SetData(offset int, data []uint16) {
-	if offset+len(data) > ibo.capacity {
-		panic("Data exceeds Index Buffer Object's capacity.")
-	}
+	ibo.check(offset, len(data), "Data")
 
 	gl.BufferSubData(gl.ELEMENT_ARRAY_BUFFER, offset, len(data)*2, gl.Ptr(data))
 }
 
 func (ibo *IndexBufferObject) Draw() {
-	statistic.Add(statistic.VerticesDrawn, int64(ibo.capacity))
-	statistic.Increment(statistic.DrawCalls)
-
-	gl.DrawElements(gl.TRIANGLES, int32(ibo.capacity), gl.UNSIGNED_SHORT, gl.PtrOffset(0))
+	ibo.DrawPart(0, ibo.capacity)
 }
 
 func (ibo *IndexBufferObject) DrawInstanced(baseInstance, instanceCount int) {
-	statistic.Add(statistic.VerticesDrawn, int64(ibo.capacity*instanceCount))
-	statistic.Increment(statistic.DrawCalls)
-
-	gl.DrawElementsInstancedBaseInstance(gl.TRIANGLES, int32(ibo.capacity), gl.UNSIGNED_SHORT, gl.PtrOffset(0), int32(instanceCount), uint32(baseInstance))
+	ibo.DrawPartInstanced(0, ibo.capacity, baseInstance, instanceCount)
 }
 
 func (ibo *IndexBufferObject) DrawPart(offset, length int) {
-	if offset+length > ibo.capacity {
-		panic("Draw exceeds Index Buffer Object's capacity.")
-	}
+	ibo.check(offset, length, "Draw")
 
 	statistic.Add(statistic.VerticesDrawn, int64(length))
 	statistic.Increment(statistic.DrawCalls)
@@ -64,9 +59,7 @@ func (ibo *IndexBufferObject) DrawPart(offset, length int) {
 }
 
 func (ibo *IndexBufferObject) DrawPartInstanced(offset, length, baseInstance, instanceCount int) {
-	if offset+length > ibo.capacity {
-		panic("Draw exceeds Index Buffer Object's capacity.")
-	}
+	ibo.check(offset, length, "Draw")
 
 	statistic.Add(statistic.VerticesDrawn, int64(length*instanceCount))
 	statistic.Increment(statistic.DrawCalls)
@@ -74,13 +67,50 @@ func (ibo *IndexBufferObject) DrawPartInstanced(offset, length, baseInstance, in
 	gl.DrawElementsInstancedBaseInstance(gl.TRIANGLES, int32(length), gl.UNSIGNED_SHORT, gl.PtrOffset(offset), int32(instanceCount), uint32(baseInstance))
 }
 
+func (ibo *IndexBufferObject) check(offset, length int, checkTarget string) {
+	currentIBO := history.GetCurrent(gl.ELEMENT_ARRAY_BUFFER_BINDING)
+	if currentIBO != ibo.handle {
+		panic(fmt.Sprintf("IBO mismatch. Target IBO: %d, current: %d", ibo.handle, currentIBO))
+	}
+
+	if offset+length > ibo.capacity {
+		panic(fmt.Sprintf("%[1]s exceeds IBO's capacity. %[1]s length: %d, offset: %d, capacity: %d", checkTarget, length, offset, ibo.capacity))
+	}
+}
+
 func (ibo *IndexBufferObject) Bind() {
+	if ibo.disposed {
+		panic("Can't bind disposed IBO")
+	}
+
+	if ibo.bound {
+		panic(fmt.Sprintf("IBO %d is already bound", ibo.handle))
+	}
+
+	ibo.bound = true
+
 	history.Push(gl.ELEMENT_ARRAY_BUFFER_BINDING)
 
 	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo.handle)
 }
 
 func (ibo *IndexBufferObject) Unbind() {
+	if !ibo.bound || ibo.disposed {
+		return
+	}
+
+	ibo.bound = false
+
 	handle := history.Pop(gl.ELEMENT_ARRAY_BUFFER_BINDING)
 	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, handle)
+}
+
+func (ibo *IndexBufferObject) Dispose() {
+	if !ibo.disposed {
+		mainthread.CallNonBlock(func() {
+			gl.DeleteBuffers(gl.ELEMENT_ARRAY_BUFFER, &ibo.handle)
+		})
+	}
+
+	ibo.disposed = true
 }
