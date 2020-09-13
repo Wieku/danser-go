b5f5c9c69739640ab90e52a4747f156a2334f650
Sebastian Krajewski <mail@wieku.me>
2020-09-13 05:57:53 +0200

    Add scissor functions to viewport helper

--- framework/graphics/viewport/viewport.go
+++ framework/graphics/viewport/viewport.go
@@ -2,7 +2,8 @@ package viewport
 
 import "github.com/go-gl/gl/v3.3-core/gl"
 
-var stack [][4]int32
+var viewportStack [][4]int32
+var scissorStack [][4]int32
 
 func Push(width, height int) {
 	PushPos(0, 0, width, height)
@@ -11,20 +12,41 @@ func Push(width, height int) {
 func PushPos(x, y, width, height int) {
 	var previous [4]int32
 	gl.GetIntegerv(gl.VIEWPORT, &previous[0])
-	stack = append(stack, previous)
+	viewportStack = append(viewportStack, previous)
 
 	gl.Viewport(int32(x), int32(y), int32(width), int32(height))
+
+	PushScissorPos(x, y, width, height)
+}
+
+func PushScissor(width, height int) {
+	PushScissorPos(0, 0, width, height)
+}
+
+func PushScissorPos(x, y, width, height int) {
+	var previous [4]int32
+	gl.GetIntegerv(gl.SCISSOR_BOX, &previous[0])
+	scissorStack = append(scissorStack, previous)
+
 	gl.Scissor(int32(x), int32(y), int32(width), int32(height))
 }
 
+func PopScissor() {
+	scissor := scissorStack[len(scissorStack)-1]
+	scissorStack = scissorStack[:len(scissorStack)-1]
+
+	gl.Scissor(scissor[0], scissor[1], scissor[2], scissor[3])
+}
+
 func Pop() {
-	viewport := stack[len(stack)-1]
-	stack = stack[:len(stack)-1]
+	PopScissor()
+
+	viewport := viewportStack[len(viewportStack)-1]
+	viewportStack = viewportStack[:len(viewportStack)-1]
 
 	gl.Viewport(viewport[0], viewport[1], viewport[2], viewport[3])
-	gl.Scissor(viewport[0], viewport[1], viewport[2], viewport[3])
 }
 
 func ClearStack() {
-	stack = stack[:0]
+	viewportStack = viewportStack[:0]
 }
