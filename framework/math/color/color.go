dc0012c3560681b2f09c2b52361ed2576885a72c
Sebastian Krajewski <mail@wieku.me>
2020-09-21 03:31:04 +0200

    Add Color type

---
+++ framework/math/color/color.go
@@ -0,0 +1,86 @@
+package color
+
+import (
+	"github.com/go-gl/mathgl/mgl32"
+	"github.com/wieku/danser-go/app/bmath"
+	"github.com/wieku/danser-go/framework/math/math32"
+	"math"
+)
+
+type Color struct {
+	R, G, B, A float32
+}
+
+func NewRGBA(r, g, b, a float32) Color {
+	return Color{r, g, b, a}
+}
+
+func NewRGB(r, g, b float32) Color {
+	return NewRGBA(r, g, b, 1.0)
+}
+
+func NewIRGBA(r, g, b, a uint8) Color {
+	return Color{float32(r) / 255, float32(g) / 255, float32(b) / 255, float32(a) / 255}
+}
+
+func NewIRGB(r, g, b uint8) Color {
+	return NewIRGBA(r, g, b, 255)
+}
+
+func NewLA(lightness, a float32) Color {
+	return NewRGBA(lightness, lightness, lightness, a)
+}
+
+func NewL(lightness float32) Color {
+	return NewLA(lightness, 1.0)
+}
+
+func NewHSVA(h, s, v, a float32) Color {
+	r, g, b := HSVToRGB(h, s, v)
+	return NewRGBA(r, g, b, a)
+}
+
+func NewHSV(h, s, v float32) Color {
+	return NewHSVA(h, s, v, 1.0)
+}
+
+func (c Color) Mix(c1 Color, t float32) Color {
+	t = bmath.ClampF32(t, 0.0, 1.0)
+	return NewRGBA(
+		c.R+(c1.R-c.R)*t,
+		c.G+(c1.G-c.G)*t,
+		c.B+(c1.B-c.B)*t,
+		c.A+(c1.A-c.A)*t,
+	)
+}
+
+func (c Color) Shift(h, s, v float32) Color {
+	h1, s1, v1 := RGBToHSV(c.R, c.G, c.B)
+
+	hR := math32.Mod(h1+h, 360)
+	if hR < 0 {
+		hR += 360
+	}
+
+	sR := bmath.ClampF32(s1+s, 0, 1)
+	vR := bmath.ClampF32(v1+v, 0, 1)
+
+	return NewHSVA(hR, sR, vR, c.A)
+}
+
+func (c Color) PackInt() uint32 {
+	rI := uint32(c.R * 255)
+	gI := uint32(c.G * 255)
+	bI := uint32(c.B * 255)
+	aI := uint32(c.A * 255)
+
+	return aI<<24 | bI<<16 | gI<<8 | rI
+}
+
+func (c Color) PackFloat() float32 {
+	return math.Float32frombits(c.PackInt())
+}
+
+func (c Color) ToVec4() mgl32.Vec4 {
+	return mgl32.Vec4{c.R, c.G, c.B, c.A}
+}
---
+++ framework/math/color/util.go
@@ -0,0 +1,74 @@
+package color
+
+import (
+	"github.com/wieku/danser-go/framework/math/math32"
+)
+
+func HSVToRGB(h, s, v float32) (r, g, b float32) {
+	hp := h / 60
+	c := v * s
+	x := c * (1.0 - math32.Abs(math32.Mod(hp, 2.0)-1.0))
+
+	m := v - c
+
+	switch {
+	case 0.0 <= hp && hp < 1.0:
+		r = c
+		g = x
+	case 1.0 <= hp && hp < 2.0:
+		r = x
+		g = c
+	case 2.0 <= hp && hp < 3.0:
+		g = c
+		b = x
+	case 3.0 <= hp && hp < 4.0:
+		g = x
+		b = c
+	case 4.0 <= hp && hp < 5.0:
+		r = x
+		b = c
+	case 5.0 <= hp && hp < 6.0:
+		r = c
+		b = x
+	}
+
+	r += m
+	g += m
+	b += m
+
+	return
+}
+
+func RGBToHSV(r, g, b float32) (h, s, v float32) {
+	min := math32.Min(math32.Min(r, g), b)
+	v = math32.Max(math32.Max(r, g), b)
+	c := v - min
+
+	s = 0.0
+	if v != 0.0 {
+		s = c / v
+	}
+
+	h = 0.0
+
+	if min != v {
+		if v == r {
+			h = math32.Mod((g-b)/c, 6.0)
+		}
+
+		if v == g {
+			h = (b-r)/c + 2.0
+		}
+
+		if v == b {
+			h = (r-g)/c + 4.0
+		}
+
+		h *= 60.0
+		if h < 0.0 {
+			h += 360.0
+		}
+	}
+
+	return
+}
