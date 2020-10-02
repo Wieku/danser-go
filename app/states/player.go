fb846fc2013935172f7b5870feb67357798c1353
Sebastian Krajewski <mail@wieku.me>
2020-10-02 05:54:47 +0200

    Whoops

--- app/states/player.go
+++ app/states/player.go
@@ -31,6 +31,7 @@ import (
 	"math"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"time"
 )
 
@@ -333,7 +334,7 @@ func NewPlayer(beatMap *beatmap.BeatMap) *Player {
 				if ov, ok := player.overlay.(*components.ScoreOverlay); ok {
 					ov.SetMusic(musicPlayer)
 				}
-				//musicPlayer.SetPosition(40)
+				//musicPlayer.SetPosition(60)
 				discord.SetDuration(int64((musicPlayer.GetLength() - musicPlayer.GetPosition()) * 1000 / settings.SPEED))
 				if player.overlay == nil {
 					discord.UpdateDance(settings.TAG, settings.DIVIDES)
