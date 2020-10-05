8563ce7d8c7d7573aa6f5e55af92f181b887debf
Sebastian Krajewski <mail@wieku.me>
2020-10-05 02:39:34 +0200

    Simplify code in bass.Sample

--- framework/bass/sample.go
+++ framework/bass/sample.go
@@ -40,11 +40,9 @@ func (wv *Sample) Play() SubSample {
 }
 
 func (wv *Sample) PlayLoop() SubSample {
-	channel := C.BASS_SampleGetChannel(C.DWORD(wv.channel), 0)
-	C.BASS_ChannelSetAttribute(channel, C.BASS_ATTRIB_VOL, C.float(settings.Audio.GeneralVolume*settings.Audio.SampleVolume))
-	C.BASS_ChannelPlay(channel, 1)
-	C.BASS_ChannelFlags(channel, C.BASS_SAMPLE_LOOP, C.BASS_SAMPLE_LOOP)
-	return SubSample(channel)
+	sSample := wv.Play()
+	C.BASS_ChannelFlags(C.HCHANNEL(sSample), C.BASS_SAMPLE_LOOP, C.BASS_SAMPLE_LOOP)
+	return sSample
 }
 
 func (wv *Sample) PlayV(volume float64) SubSample {
@@ -55,12 +53,24 @@ func (wv *Sample) PlayV(volume float64) SubSample {
 	return SubSample(channel)
 }
 
+func (wv *Sample) PlayVLoop(volume float64) SubSample {
+	sSample := wv.PlayV(volume)
+	C.BASS_ChannelFlags(C.HCHANNEL(sSample), C.BASS_SAMPLE_LOOP, C.BASS_SAMPLE_LOOP)
+	return sSample
+}
+
 func (wv *Sample) PlayRV(volume float64) SubSample {
 	channel := C.BASS_SampleGetChannel(C.DWORD(wv.channel), 0)
 	C.BASS_ChannelSetAttribute(channel, C.BASS_ATTRIB_VOL, C.float(settings.Audio.GeneralVolume*settings.Audio.SampleVolume*volume))
 	C.BASS_ChannelPlay(channel, 1)
 
 	return SubSample(channel)
+}
+
+func (wv *Sample) PlayRVLoop(volume float64) SubSample {
+	sSample := wv.PlayRV(volume)
+	C.BASS_ChannelFlags(C.HCHANNEL(sSample), C.BASS_SAMPLE_LOOP, C.BASS_SAMPLE_LOOP)
+	return sSample
 }
 
 func (wv *Sample) PlayRVPos(volume float64, balance float64) SubSample {
@@ -70,6 +80,12 @@ func (wv *Sample) PlayRVPos(volume float64, balance float64) SubSample {
 	C.BASS_ChannelPlay(channel, 1)
 
 	return SubSample(channel)
+}
+
+func (wv *Sample) PlayRVPosLoop(volume float64, balance float64) SubSample {
+	sSample := wv.PlayRVPos(volume, balance)
+	C.BASS_ChannelFlags(C.HCHANNEL(sSample), C.BASS_SAMPLE_LOOP, C.BASS_SAMPLE_LOOP)
+	return sSample
 }
 
 func SetRate(channel SubSample, rate float64) {
