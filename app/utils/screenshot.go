19a4c6afcd9bcb2c1a9d9fb7c64eaceff55c4f72
Sebastian Krajewski <mail@wieku.me>
2020-12-07 01:37:03 +0100

    Make screenshot save a little bit faster

--- app/utils/screenshot.go
+++ app/utils/screenshot.go
@@ -3,41 +3,122 @@ package utils
 import (
 	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
-	"image"
-	"image/png"
+	"github.com/wieku/danser-go/framework/graphics/texture"
+	"log"
 	"os"
-	"runtime"
 	"time"
-	"unsafe"
 )
 
 func MakeScreenshot(win glfw.Window) {
 	w, h := win.GetFramebufferSize()
-	buff := make([]uint8, w*h*4)
+
+	pixmap := texture.NewPixMapC(w, h, 3)
+
 	gl.PixelStorei(gl.PACK_ALIGNMENT, int32(1))
-	gl.ReadPixels(0, 0, int32(w), int32(h), gl.RGBA, gl.UNSIGNED_BYTE, unsafe.Pointer(&buff[0]))
+	gl.ReadPixels(0, 0, int32(w), int32(h), gl.RGB, gl.UNSIGNED_BYTE, pixmap.RawPointer)
 
 	go func() {
-		img := image.NewNRGBA(image.Rectangle{Min: image.Point{0, 0}, Max: image.Point{w, h}})
-		buff1 := make([]uint8, w*h*4)
-		for i := h - 1; i >= 0; i-- {
-			for j := 0; j < w*4; j++ {
-				if (j+1)%4 == 0 {
-					buff1[(h-1-i)*w*4+j] = 0xFF
-				} else {
-					buff1[(h-1-i)*w*4+j] = buff[i*w*4+j]
-				}
-			}
+		defer pixmap.Dispose()
+
+		err := os.Mkdir("screenshots", 0644)
+		if !os.IsExist(err) {
+			log.Println("Failed to save the screenshot! Error:", err)
+			return
 		}
-		runtime.KeepAlive(buff)
-		img.Pix = buff1
-		os.Mkdir("screenshots", 0644)
 
-		dateString := time.Now().Format("2006-01-02_15-04-05")
-		f, _ := os.OpenFile("screenshots/danser_"+dateString+".png", os.O_WRONLY|os.O_CREATE, 0644)
+		fileName := "danser_" + time.Now().Format("2006-01-02_15-04-05") + ".png"
 
-		defer f.Close()
+		err = pixmap.WritePng("screenshots/"+fileName, true)
+		if err != nil {
+			log.Println("Failed to save the screenshot! Error:", err)
+			return
+		}
 
-		png.Encode(f, img)
+		log.Println("Screenshot", fileName, "saved!")
 	}()
 }
+
+//var cmd *exec.Cmd
+//var pipe io.WriteCloser
+//
+//var FFMpegStart int64
+//
+//func StartFFmpeg(fps, w, h int) {
+//	os.Mkdir("videos", 0655)
+//	cmd = exec.Command("ffmpeg",
+//		"-y", //(optional) overwrite output file if it exists
+//		"-f", "rawvideo",
+//		"-vcodec","rawvideo",
+//		"-s", fmt.Sprintf("%dx%d", w, h), //size of one frame
+//		"-pix_fmt", "rgb24",
+//		"-r", strconv.Itoa(fps), //frames per second
+//		"-i", "-", //The imput comes from a pipe
+//		"-vf", "vflip",
+//		"-an", //Tells FFMPEG not to expect any audio
+//		"-vcodec", "h264_nvenc",
+//		"-rc", "constqp",
+//		"-qp", "15",
+//		"-threads", "4",
+//		"videos/video.mp4",
+//	)
+//
+//	cmd.Stdout = os.Stdout
+//	cmd.Stderr = os.Stderr
+//
+//	var err error
+//
+//	pipe, err = cmd.StdinPipe()
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	err = cmd.Start()
+//	if err != nil {
+//		panic(err)
+//	}
+//}
+//
+//func StopFFmpeg() {
+//	pipe.Close()
+//	//cmd.Process.Signal(os.Kill)
+//	cmd.Wait()
+//}
+//
+//func Combine() {
+//	cmd2 := exec.Command("ffmpeg",
+//		"-y",
+//		"-i", "videos/video.mp4",
+//		"-i", "videos/audio.wav",
+//		"-c:v", "copy",
+//		"-c:a", "aac",
+//		"-ab", "320k",
+//		"videos/danser_"+time.Now().Format("2006-01-02_15-04-05")+".mp4",
+//	)
+//	cmd2.Start()
+//	cmd2.Wait()
+//	os.Remove("videos/video.mp4")
+//	os.Remove("videos/audio.wav")
+//}
+//
+//func MakeFrame(win *glfw.Window, num int) {
+//	if num == 0 {
+//		FFMpegStart = qpc.GetNanoTime()
+//	}
+//
+//	w, h := win.GetFramebufferSize()
+//
+//	pixmap := texture.NewPixMapC(w, h, 3)
+//
+//	gl.PixelStorei(gl.PACK_ALIGNMENT, int32(1))
+//	gl.ReadPixels(0, 0, int32(w), int32(h), gl.RGB, gl.UNSIGNED_BYTE, pixmap.RawPointer)
+//
+//	mainthread.CallNonBlock(func() {
+//		_, err := pipe.Write(pixmap.Data)
+//		if err != nil {
+//			panic(err)
+//		}
+//		//log.Println("sent frame", num)
+//
+//		pixmap.Dispose()
+//	})
+//}
