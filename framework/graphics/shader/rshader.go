021f478019ec8506506e81df37e382ea87f7c6f7
Sebastian Krajewski <mail@wieku.me>
2020-12-31 17:27:48 +0100

    Derp

--- framework/graphics/shader/rshader.go
+++ framework/graphics/shader/rshader.go
@@ -8,6 +8,7 @@ import (
 	"github.com/wieku/danser-go/framework/graphics/attribute"
 	"github.com/wieku/danser-go/framework/graphics/history"
 	"github.com/wieku/danser-go/framework/math/color"
+	"log"
 	"runtime"
 )
 
@@ -141,6 +142,7 @@ func (s *RShader) GetUniformInfo(name string) attribute.VertexAttribute {
 func (s *RShader) SetUniform(name string, value interface{}) {
 	uniform, exists := s.uniforms[name]
 	if !exists {
+		log.Println(s.uniforms)
 		panic(fmt.Sprintf("Uniform %s doesn't exist", name))
 	}
 
@@ -200,6 +202,74 @@ func (s *RShader) SetUniform(name string, value interface{}) {
 
 		value := value.(int32)
 		gl.ProgramUniform1iv(s.handle, uniform.Location, 1, &value)
+	}
+}
+
+func (s *RShader) SetUniformArr(name string, offset int, value interface{}) {
+	name += "[0]"
+
+	uniform, exists := s.uniforms[name]
+	if !exists {
+		log.Println(s.uniforms)
+		panic(fmt.Sprintf("Uniform %s doesn't exist", name))
+	}
+
+	switch uniform.Type {
+	case attribute.Float:
+		value := value.(float32)
+		gl.ProgramUniform1fv(s.handle, uniform.Location+int32(offset), 1, &value)
+	case attribute.Vec2:
+		value := value.(mgl32.Vec2)
+		gl.ProgramUniform2fv(s.handle, uniform.Location+int32(offset), 1, &value[0])
+	case attribute.Vec3:
+		value := value.(mgl32.Vec3)
+		gl.ProgramUniform3fv(s.handle, uniform.Location+int32(offset), 1, &value[0])
+	case attribute.Vec4:
+		if c, ok := value.(color.Color); ok {
+			gl.ProgramUniform4fv(s.handle, uniform.Location+int32(offset), 1, &c.ToArray()[0])
+
+			break
+		}
+
+		value := value.(mgl32.Vec4)
+		gl.ProgramUniform4fv(s.handle, uniform.Location+int32(offset), 1, &value[0])
+	case attribute.Mat2:
+		value := value.(mgl32.Mat2)
+		gl.ProgramUniformMatrix2fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	case attribute.Mat23:
+		value := value.(mgl32.Mat2x3)
+		gl.ProgramUniformMatrix2x3fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	case attribute.Mat24:
+		value := value.(mgl32.Mat2x4)
+		gl.ProgramUniformMatrix2x4fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	case attribute.Mat3:
+		value := value.(mgl32.Mat3)
+		gl.ProgramUniformMatrix3fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	case attribute.Mat32:
+		value := value.(mgl32.Mat3x2)
+		gl.ProgramUniformMatrix3x2fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	case attribute.Mat34:
+		value := value.(mgl32.Mat3x4)
+		gl.ProgramUniformMatrix3x4fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	case attribute.Mat4:
+		value := value.(mgl32.Mat4)
+		gl.ProgramUniformMatrix4fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	case attribute.Mat42:
+		value := value.(mgl32.Mat4x2)
+		gl.ProgramUniformMatrix4x2fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	case attribute.Mat43:
+		value := value.(mgl32.Mat4x3)
+		gl.ProgramUniformMatrix4x3fv(s.handle, uniform.Location+int32(offset), 1, false, &value[0])
+	default: // We assume that uniform is of type int or sampler
+		if value, ok := value.(int); ok {
+			value := int32(value)
+			gl.ProgramUniform1iv(s.handle, uniform.Location+int32(offset), 1, &value)
+
+			break
+		}
+
+		value := value.(int32)
+		gl.ProgramUniform1iv(s.handle, uniform.Location+int32(offset), 1, &value)
 	}
 }
 
