939d560f94664b1d28e4e48c1d2ffbfc744c0d7c
Sebastian Krajewski <mail@wieku.me>
2021-02-02 05:25:24 +0100

    Don't log hit judgements while recording

--- app/rulesets/osu/ruleset.go
+++ app/rulesets/osu/ruleset.go
@@ -497,7 +497,7 @@ func (set *OsuRuleSet) SendResult(time int64, cursor *graphics.Cursor, number in
 		set.listener(cursor, time, number, vector.NewVec2f(x, y).Copy64(), result, comboResult, subSet.ppv2.Total, subSet.score)
 	}
 
-	if len(set.cursors) == 1 {
+	if len(set.cursors) == 1 && !settings.RECORD {
 		log.Println(fmt.Sprintf(
 			"Got: %3d, Combo: %4d, Max Combo: %4d, Score: %9d, Acc: %6.2f%%, 300: %4d, 100: %3d, 50: %2d, miss: %2d, from: %d, at: %d, pos: %.0fx%.0f, pp: %.2f",
 			result.ScoreValue(),
