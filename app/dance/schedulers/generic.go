d10e99e392fa4fbe7a18458d19ba72934ed0d48c
Sebastian Krajewski <mail@wieku.me>
2021-03-20 19:58:50 +0100

    Fix ExGon mover not hitting circles

--- app/dance/schedulers/generic.go
+++ app/dance/schedulers/generic.go
@@ -26,34 +26,34 @@ func NewGenericScheduler(mover func() movers.MultiPointMover) Scheduler {
 	return &GenericScheduler{mover: mover()}
 }
 
-func (sched *GenericScheduler) Init(objs []objects.IHitObject, mods difficulty.Modifier, cursor *graphics.Cursor, spinnerMover spinners.SpinnerMover, initKeys bool) {
-	sched.mods = mods
-	sched.spinnerMover = spinnerMover
-	sched.cursor = cursor
-	sched.queue = objs
+func (scheduler *GenericScheduler) Init(objs []objects.IHitObject, mods difficulty.Modifier, cursor *graphics.Cursor, spinnerMover spinners.SpinnerMover, initKeys bool) {
+	scheduler.mods = mods
+	scheduler.spinnerMover = spinnerMover
+	scheduler.cursor = cursor
+	scheduler.queue = objs
 
 	if initKeys {
-		sched.input = input.NewNaturalInputProcessor(objs, cursor)
+		scheduler.input = input.NewNaturalInputProcessor(objs, cursor)
 	}
 
-	sched.mover.Reset(mods)
+	scheduler.mover.Reset(mods)
 
-	for i := 0; i < len(sched.queue); i++ {
-		sched.queue = PreprocessQueue(i, sched.queue, (settings.Dance.SliderDance && !settings.Dance.RandomSliderDance) || (settings.Dance.RandomSliderDance && rand.Intn(2) == 0))
+	for i := 0; i < len(scheduler.queue); i++ {
+		scheduler.queue = PreprocessQueue(i, scheduler.queue, (settings.Dance.SliderDance && !settings.Dance.RandomSliderDance) || (settings.Dance.RandomSliderDance && rand.Intn(2) == 0))
 	}
 
-	sched.queue = append([]objects.IHitObject{objects.DummyCircle(vector.NewVec2f(100, 100), 0)}, sched.queue...)
+	scheduler.queue = append([]objects.IHitObject{objects.DummyCircle(vector.NewVec2f(100, 100), 0)}, scheduler.queue...)
 
-	toRemove := sched.mover.SetObjects(sched.queue) - 1
-	sched.queue = sched.queue[toRemove:]
+	toRemove := scheduler.mover.SetObjects(scheduler.queue) - 1
+	scheduler.queue = scheduler.queue[toRemove:]
 }
 
-func (sched *GenericScheduler) Update(time float64) {
-	if len(sched.queue) > 0 {
+func (scheduler *GenericScheduler) Update(time float64) {
+	if len(scheduler.queue) > 0 {
 		move := true
 
-		for i := 0; i < len(sched.queue); i++ {
-			g := sched.queue[i]
+		for i := 0; i < len(scheduler.queue); i++ {
+			g := scheduler.queue[i]
 
 			if g.GetStartTime() > time {
 				break
@@ -61,37 +61,37 @@ func (sched *GenericScheduler) Update(time float64) {
 
 			move = false
 
-			if time >= g.GetStartTime() && time <= g.GetEndTime() {
+			if (scheduler.lastTime <= g.GetStartTime() && time >= g.GetStartTime()) || (time >= g.GetStartTime() && time <= g.GetEndTime()) {
 				if _, ok := g.(*objects.Spinner); ok {
-					if sched.lastTime < g.GetStartTime() {
-						sched.spinnerMover.Init(g.GetStartTime(), g.GetEndTime())
+					if scheduler.lastTime < g.GetStartTime() {
+						scheduler.spinnerMover.Init(g.GetStartTime(), g.GetEndTime())
 					}
 
-					sched.cursor.SetPos(sched.spinnerMover.GetPositionAt(time))
+					scheduler.cursor.SetPos(scheduler.spinnerMover.GetPositionAt(time))
 				} else {
-					sched.cursor.SetPos(g.GetStackedPositionAtMod(time, sched.mods))
+					scheduler.cursor.SetPos(g.GetStackedPositionAtMod(time, scheduler.mods))
 				}
 			} else if time > g.GetEndTime() {
 				toRemove := 1
-				if i+1 < len(sched.queue) {
-					toRemove = sched.mover.SetObjects(sched.queue[i:]) - 1
+				if i+1 < len(scheduler.queue) {
+					toRemove = scheduler.mover.SetObjects(scheduler.queue[i:]) - 1
 				}
 
-				sched.queue = append(sched.queue[:i], sched.queue[i+toRemove:]...)
+				scheduler.queue = append(scheduler.queue[:i], scheduler.queue[i+toRemove:]...)
 				i--
 
 				move = true
 			}
 		}
 
-		if move && sched.mover.GetEndTime() >= time {
-			sched.cursor.SetPos(sched.mover.Update(time))
+		if move && scheduler.mover.GetEndTime() >= time {
+			scheduler.cursor.SetPos(scheduler.mover.Update(time))
 		}
 	}
 
-	if sched.input != nil {
-		sched.input.Update(time)
+	if scheduler.input != nil {
+		scheduler.input.Update(time)
 	}
 
-	sched.lastTime = time
+	scheduler.lastTime = time
 }
