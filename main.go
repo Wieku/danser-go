599f67171ca842cf2e6ea29f50ebdfcc67f3fe1f
Sebastian Krajewski <mail@wieku.me>
2021-05-13 05:30:02 +0200

    Force high performance GPU on Windows (AMD and NVIDIA)

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+/*
+#ifdef _WIN32
+#include <windows.h>
+// force switch to the high performance gpu in multi-gpu systems (mostly laptops)
+__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001; // http://developer.download.nvidia.com/devzone/devcenter/gamegraphics/files/OptimusRenderingPolicies.pdf
+__declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001; // https://community.amd.com/thread/169965
+#endif
+ */
 import "C"
+
 import (
 	"encoding/json"
 	"flag"
