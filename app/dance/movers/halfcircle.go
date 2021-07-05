60c633e3cf41a55a069b5d7bae57d4fedc9152f5
Sebastian Krajewski <mail@wieku.me>
2021-07-05 05:55:07 +0200

    Clean up halfcircle mover a bit

--- app/dance/movers/halfcircle.go
+++ app/dance/movers/halfcircle.go
@@ -13,9 +13,11 @@ import (
 type HalfCircleMover struct {
 	*basicMover
 
-	ca      curves.Curve
+	curve curves.Curve
+
 	startTime float64
-	invert  float32
+
+	invert float32
 }
 
 func NewHalfCircleMover() MultiPointMover {
@@ -31,31 +33,30 @@ func (mover *HalfCircleMover) Reset(diff *difficulty.Difficulty, id int) {
 func (mover *HalfCircleMover) SetObjects(objs []objects.IHitObject) int {
 	config := settings.CursorDance.MoverSettings.HalfCircle[mover.id%len(settings.CursorDance.MoverSettings.HalfCircle)]
 
-	start := objs[0]
-	end := objs[1]
+	start, end := objs[0], objs[1]
+
+	mover.startTime = start.GetEndTime()
+	mover.endTime = end.GetStartTime()
 
 	startPos := start.GetStackedEndPositionMod(mover.diff.Mods)
 	endPos := end.GetStackedStartPositionMod(mover.diff.Mods)
-	mover.startTime = start.GetEndTime()
-	mover.endTime = end.GetStartTime()
 
 	if config.StreamTrigger < 0 || (mover.endTime-mover.startTime) < float64(config.StreamTrigger) {
 		mover.invert = -1 * mover.invert
 	}
 
 	if startPos == endPos {
-		mover.ca = curves.NewLinear(startPos, endPos)
-		return 2
+		mover.curve = curves.NewLinear(startPos, endPos)
+	} else {
+		point := startPos.Mid(endPos)
+		p := point.Sub(startPos).Rotate(mover.invert * math.Pi / 2).Scl(float32(config.RadiusMultiplier)).Add(point)
+		mover.curve = curves.NewCirArc(startPos, p, endPos)
 	}
-
-	point := startPos.Mid(endPos)
-	p := point.Sub(startPos).Rotate(mover.invert * math.Pi / 2).Scl(float32(config.RadiusMultiplier)).Add(point)
-	mover.ca = curves.NewCirArc(startPos, p, endPos)
 
 	return 2
 }
 
 func (mover *HalfCircleMover) Update(time float64) vector.Vector2f {
-	t := bmath.ClampF32(float32(time-mover.startTime)/float32(mover.endTime-mover.startTime), 0, 1)
-	return mover.ca.PointAt(t)
+	t := bmath.ClampF64((time-mover.startTime)/(mover.endTime-mover.startTime), 0, 1)
+	return mover.curve.PointAt(float32(t))
 }
