6f86b4ea2e11ad9a76a28c91d250da94f705b971
Sebastian Krajewski <mail@wieku.me>
2021-07-13 03:19:03 +0200

    Add BSpline solver

--- framework/math/curves/bspline.go
+++ framework/math/curves/bspline.go
@@ -117,3 +117,51 @@ func (spline *BSpline) GetStartAngle() float32 {
 func (spline *BSpline) GetEndAngle() float32 {
 	return spline.points[len(spline.points)-1].AngleRV(spline.PointAt((spline.ApproxLength - 1) / spline.ApproxLength))
 }
+
+// SolveBSpline calculates the spline that goes through all given control points.
+// points[1] and points[len(points)-2] are terminal tangents
+// Returns an array of bezier curves in NA (non-approximated) version for performance considerations.
+func SolveBSpline(points1 []vector.Vector2f) []*Bezier {
+	pointsLen := len(points1)
+
+	points := make([]vector.Vector2f, 0, pointsLen)
+
+	points = append(points, points1[0])
+	points = append(points, points1[2:pointsLen-2]...)
+	points = append(points, points1[pointsLen-1], points1[1], points1[pointsLen-2])
+
+	n := len(points) - 2
+
+	d := make([]vector.Vector2f, n)
+	d[0] = points[n].Sub(points[0])
+	d[n-1] = points[n+1].Sub(points[n-1]).Scl(-1)
+
+	A := make([]vector.Vector2f, len(points))
+	Bi := make([]float32, len(points))
+
+	Bi[1] = -0.25
+	A[1] = points[2].Sub(points[0]).Sub(d[0]).Scl(1.0 / 4)
+	for i := 2; i < n-1; i++ {
+		Bi[i] = -1 / (4 + Bi[i-1])
+		A[i] = points[i+1].Sub(points[i-1]).Sub(A[i-1]).Scl(-1 * Bi[i])
+	}
+
+	for i := n - 2; i > 0; i-- {
+		d[i] = A[i].Add(d[i+1].Scl(Bi[i]))
+	}
+
+	subPoints := []vector.Vector2f{points[0], points[0].Add(d[0])}
+
+	for i := 1; i < n-1; i++ {
+		subPoints = append(subPoints, points[i].Sub(d[i]), points[i], points[i].Add(d[i]))
+	}
+
+	subPoints = append(subPoints, points[n-1].Sub(d[n-1]), points[n-1])
+
+	var beziers []*Bezier
+	for i := 0; i < len(subPoints)-3; i += 3 {
+		beziers = append(beziers, NewBezierNA(subPoints[i:i+4]))
+	}
+
+	return beziers
+}
