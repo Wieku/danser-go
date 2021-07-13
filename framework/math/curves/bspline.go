e99a951ecebdbbe780e59f4284bc9a8b8e1129de
Sebastian Krajewski <mail@wieku.me>
2021-07-13 03:42:35 +0200

    Utilize spline solver in BSpline constructor

--- framework/math/curves/bspline.go
+++ framework/math/curves/bspline.go
@@ -1,109 +1,62 @@
 package curves
 
 import (
+	"github.com/wieku/danser-go/app/bmath"
 	"github.com/wieku/danser-go/framework/math/vector"
+	"sort"
 )
 
 type BSpline struct {
 	points       []vector.Vector2f
-	timing       []float32
-	subPoints    []vector.Vector2f
+	sections     []float32
 	path         []*Bezier
 	ApproxLength float32
 }
 
-func NewBSpline(points1 []vector.Vector2f, timing []int64) *BSpline {
-
-	pointsLen := len(points1)
-
-	points := make([]vector.Vector2f, 0)
-
-	points = append(points, points1[0])
-	points = append(points, points1[2:pointsLen-2]...)
-	points = append(points, points1[pointsLen-1], points1[1], points1[pointsLen-2])
-
+func NewBSpline(points []vector.Vector2f, timing []int64) *BSpline {
 	newTiming := make([]float32, len(timing))
 	for i := range newTiming {
 		newTiming[i] = float32(timing[i] - timing[0])
 	}
 
-	spline := &BSpline{points: points, timing: newTiming}
-
-	n := len(points) - 2
-
-	d := make([]vector.Vector2f, n)
-	d[0] = points[n].Sub(points[0])
-	d[n-1] = points[n+1].Sub(points[n-1]).Scl(-1)
-
-	A := make([]vector.Vector2f, len(points))
-	Bi := make([]float32, len(points))
-
-	Bi[1] = -0.25
-	A[1] = points[2].Sub(points[0]).Sub(d[0]).Scl(1.0 / 4)
-	for i := 2; i < n-1; i++ {
-		Bi[i] = -1 / (4 + Bi[i-1])
-		A[i] = points[i+1].Sub(points[i-1]).Sub(A[i-1]).Scl(-1 * Bi[i])
+	spline := &BSpline{
+		points:   points,
+		sections: newTiming,
+		path:     SolveBSpline(points),
 	}
 
-	for i := n - 2; i > 0; i-- {
-		d[i] = A[i].Add(d[i+1].Scl(Bi[i]))
+	for i, b := range spline.path {
+		d := spline.sections[i+1] - spline.sections[i]
 
-	}
-
-	converted := make([]float32, len(timing))
-
-	for i, time := range timing {
-		if i > 0 {
-			converted[i-1] = float32(time - timing[i-1])
-		}
-	}
-
-	firstMul := float32(1.0)
-	if converted[0] > 600 {
-		firstMul = converted[0] / 2
-	}
-
-	secondMul := float32(1.0)
-
-	spline.subPoints = append(spline.subPoints, points[0], points[0].Add(d[0].SclOrDenorm(firstMul)))
-
-	for i := 1; i < n-1; i++ {
-		if converted[i] > 600 {
-			secondMul = converted[i] / 2
-		} else {
-			secondMul = 1.0
+		scl := float32(1.0)
+		if d > 600 {
+			scl = d / 2
 		}
 
-		spline.subPoints = append(spline.subPoints, points[i].Sub(d[i].SclOrDenorm(firstMul)), points[i], points[i].Add(d[i].SclOrDenorm(secondMul)))
-		firstMul = secondMul
+		b.Points[1] = b.Points[0].Add(b.Points[1].Sub(b.Points[0]).SclOrDenorm(scl))
+		b.Points[2] = b.Points[3].Add(b.Points[2].Sub(b.Points[3]).SclOrDenorm(scl))
 	}
 
-	spline.subPoints = append(spline.subPoints, points[len(points)-3].Sub(d[n-1].SclOrDenorm(firstMul)), points[len(points)-3])
-
-	spline.ApproxLength = spline.timing[len(spline.timing)-1]
-
-	for i := 0; i < len(spline.subPoints)-3; i += 3 {
-		c := NewBezierNA(spline.subPoints[i : i+4])
-		spline.path = append(spline.path, c)
-	}
+	spline.ApproxLength = spline.sections[len(spline.sections)-1]
 
 	return spline
 }
 
 func (spline *BSpline) PointAt(t float32) vector.Vector2f {
-	desiredWidth := spline.ApproxLength * t
+	desiredWidth := spline.ApproxLength * bmath.ClampF32(t, 0.0, 1.0)
 
-	lineI := len(spline.timing) - 2
+	withoutFirst := spline.sections[1:]
+	index := sort.Search(len(withoutFirst), func(i int) bool {
+		return withoutFirst[i] >= desiredWidth
+	})
 
-	for i, k := range spline.timing[:len(spline.timing)-1] {
-		if k <= desiredWidth {
-			lineI = i
-		}
+	index = bmath.MinI(index, len(spline.path)-1)
+
+	if spline.sections[index+1]-spline.sections[index] == 0 {
+		return spline.path[index].PointAt(0)
 	}
 
-	line := spline.path[lineI]
-
-	return line.PointAt((desiredWidth - spline.timing[lineI]) / (spline.timing[lineI+1] - spline.timing[lineI]))
+	return spline.path[index].PointAt((desiredWidth - spline.sections[index]) / (spline.sections[index+1] - spline.sections[index]))
 }
 
 func (spline *BSpline) GetLength() float32 {
@@ -111,11 +64,11 @@ func (spline *BSpline) GetLength() float32 {
 }
 
 func (spline *BSpline) GetStartAngle() float32 {
-	return spline.points[0].AngleRV(spline.PointAt(1.0 / spline.ApproxLength))
+	return spline.path[0].GetStartAngle()
 }
 
 func (spline *BSpline) GetEndAngle() float32 {
-	return spline.points[len(spline.points)-1].AngleRV(spline.PointAt((spline.ApproxLength - 1) / spline.ApproxLength))
+	return spline.path[len(spline.path)-1].GetEndAngle()
 }
 
 // SolveBSpline calculates the spline that goes through all given control points.
