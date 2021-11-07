fc7c9ff6312a22a3de87c265360f7b9be67336a9
Sebastian Krajewski <mail@wieku.me>
2021-11-07 19:12:33 +0100

    Port new DifficultyObject calculations

--- app/rulesets/osu/performance/preprocessing/object.go
+++ app/rulesets/osu/performance/preprocessing/object.go
@@ -9,9 +9,10 @@ import (
 )
 
 const (
-	NormalizedRadius        = 52.0
+	NormalizedRadius        = 50.0
 	CircleSizeBuffThreshold = 30.0
 	OsuStableAllowance      = 1.00041
+	MinDeltaTime            = 25
 )
 
 type DifficultyObject struct {
@@ -31,9 +32,15 @@ type DifficultyObject struct {
 
 	JumpDistance float64
 
+	MovementDistance float64
+
 	TravelDistance float64
 
 	Angle float64
+
+	MovementTime float64
+
+	TravelTime float64
 
 	StrainTime float64
 }
@@ -49,9 +56,9 @@ func NewDifficultyObject(hitObject, lastLastObject, lastObject objects.IHitObjec
 		EndTime:        hitObject.GetEndTime() / d.Speed,
 	}
 
-	obj.setDistances(experimental)
+	obj.StrainTime = math.Max(obj.DeltaTime, MinDeltaTime)
 
-	obj.StrainTime = math.Max(25, obj.DeltaTime)
+	obj.setDistances(experimental)
 
 	return obj
 }
@@ -72,13 +79,27 @@ func (o *DifficultyObject) setDistances(experimental bool) {
 			math32.Min(CircleSizeBuffThreshold-float32(radius), 5.0)/50.0
 	}
 
-	if s, ok := o.lastObject.(*LazySlider); ok {
-		o.TravelDistance = float64(s.LazyTravelDistance * scalingFactor)
-	}
-
 	lastCursorPosition := getEndCursorPosition(o.lastObject, o.diff)
-
 	o.JumpDistance = float64((o.BaseObject.GetStackedStartPositionMod(o.diff.Mods).Scl(scalingFactor)).Dst(lastCursorPosition.Scl(scalingFactor)))
+
+	if lastSlider, ok := o.lastObject.(*LazySlider); ok {
+		o.TravelDistance = float64(lastSlider.LazyTravelDistance)
+		o.TravelTime = math.Max(lastSlider.LazyTravelTime/o.diff.Speed, MinDeltaTime)
+		o.MovementTime = math.Max(o.StrainTime-o.TravelTime, MinDeltaTime)
+
+		// Jump distance from the slider tail to the next object, as opposed to the lazy position of JumpDistance.
+		tailJumpDistance := lastSlider.GetStackedEndPositionMod(o.diff.Mods).Dst(o.BaseObject.GetStackedStartPositionMod(o.diff.Mods)) * scalingFactor
+
+		// For hitobjects which continue in the direction of the slider, the player will normally follow through the slider,
+		// such that they're not jumping from the lazy position but rather from very close to (or the end of) the slider.
+		// In such cases, a leniency is applied by also considering the jump distance from the tail of the slider, and taking the minimum jump distance.
+		// Additional distance is removed based on position of jump relative to slider follow circle radius.
+		// JumpDistance is the leniency distance beyond the assumed_slider_radius. tailJumpDistance is maximum_slider_radius since the full distance of radial leniency is still possible.
+		o.MovementDistance = math.Max(0, math.Min(o.JumpDistance-(maximumSliderRadius-assumedSliderRadius), float64(tailJumpDistance-maximumSliderRadius)))
+	} else {
+		o.MovementTime = o.StrainTime
+		o.MovementDistance = o.JumpDistance
+	}
 
 	if o.lastLastObject != nil {
 		if _, ok := o.lastLastObject.(*objects.Spinner); ok {
