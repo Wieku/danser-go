d31342eba2836d562ff5c85b59da94fe9796688f
Sebastian Krajewski <mail@wieku.me>
2022-03-22 02:53:48 +0100

    Add short revision hash to dev build version at runtime

--- build/version.go
+++ build/version.go
@@ -1,5 +1,28 @@
 package build
 
+import (
+	"github.com/wieku/danser-go/framework/math/mutils"
+	"runtime/debug"
+)
+
 var VERSION = "dev"
 
 var Stream = "Dev"
+
+func init() {
+	if VERSION == "dev" {
+		if bI, ok := debug.ReadBuildInfo(); ok {
+			rev := ""
+
+			for _, k := range bI.Settings {
+				if k.Key == "vcs.revision" {
+					rev = k.Value
+				}
+			}
+
+			if rev != "" {
+				VERSION += "-" + rev[:mutils.Min(7, len(rev))]
+			}
+		}
+	}
+}
