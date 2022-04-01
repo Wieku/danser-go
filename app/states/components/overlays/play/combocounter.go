d2350ac08e463c7624c0daef860629a2c039ed32
Sebastian Krajewski <mail@wieku.me>
2022-04-01 06:27:30 +0200

    Switch to TextSprite, make slide animation more stable-like

--- app/states/components/overlays/play/combocounter.go
+++ app/states/components/overlays/play/combocounter.go
@@ -8,6 +8,7 @@ import (
 	"github.com/wieku/danser-go/framework/bass"
 	"github.com/wieku/danser-go/framework/graphics/batch"
 	"github.com/wieku/danser-go/framework/graphics/font"
+	"github.com/wieku/danser-go/framework/graphics/sprite"
 	"github.com/wieku/danser-go/framework/math/animation"
 	"github.com/wieku/danser-go/framework/math/animation/easing"
 	"github.com/wieku/danser-go/framework/math/vector"
@@ -17,12 +18,12 @@ import (
 type ComboCounter struct {
 	comboFont *font.Font
 
-	comboSlide     *animation.Glider
-	newComboScale  *animation.Glider
-	newComboScaleB *animation.Glider
-	newComboFadeB  *animation.Glider
+	mainCounter *sprite.TextSprite
+	popCounter  *sprite.TextSprite
 
-	combobreak *bass.Sample
+	comboSlide *animation.Glider
+
+	comboBreak *bass.Sample
 
 	time    float64
 	delta   float64
@@ -38,14 +39,21 @@ type ComboCounter struct {
 }
 
 func NewComboCounter() *ComboCounter {
+	fnt := skin.GetFont("combo")
+
 	counter := &ComboCounter{
-		comboFont:      skin.GetFont("combo"),
-		comboSlide:     animation.NewGlider(0),
-		newComboScale:  animation.NewGlider(1.28),
-		newComboScaleB: animation.NewGlider(1.28),
-		newComboFadeB:  animation.NewGlider(0),
-		combobreak:     audio.LoadSample("combobreak"),
+		comboFont:   fnt,
+		mainCounter: sprite.NewTextSprite("0x", fnt, 0, vector.NewVec2d(0, 0), vector.BottomLeft),
+		popCounter:  sprite.NewTextSprite("0x", fnt, 0, vector.NewVec2d(0, 0), vector.BottomLeft),
+		comboSlide:  animation.NewGlider(0),
+		comboBreak:  audio.LoadSample("combobreak"),
 	}
+
+	counter.mainCounter.SetScale(1.28)
+
+	counter.popCounter.SetScale(1.28)
+	counter.popCounter.SetAlpha(0)
+	counter.popCounter.SetAdditive(true)
 
 	counter.ScaledHeight = 768
 	counter.ScaledWidth = settings.Graphics.GetAspectRatio() * counter.ScaledHeight
@@ -56,11 +64,9 @@ func NewComboCounter() *ComboCounter {
 }
 
 func (counter *ComboCounter) Increase() {
-	counter.newComboScaleB.Reset()
-	counter.newComboScaleB.AddEventS(counter.time, counter.time+300, 2, 1.28)
-
-	counter.newComboFadeB.Reset()
-	counter.newComboFadeB.AddEventS(counter.time, counter.time+300, 0.6, 0.0)
+	counter.popCounter.ClearTransformations()
+	counter.popCounter.AddTransform(animation.NewSingleTransform(animation.Scale, easing.Linear, counter.time, counter.time+300, 1.563, 1))
+	counter.popCounter.AddTransform(animation.NewSingleTransform(animation.Fade, easing.Linear, counter.time, counter.time+300, 0.6, 0.0))
 
 	if counter.combo < counter.newCombo {
 		counter.animate(counter.time)
@@ -69,14 +75,18 @@ func (counter *ComboCounter) Increase() {
 	counter.combo = counter.newCombo
 	counter.newCombo++
 	counter.nextEnd = counter.time + 300
+
+	counter.popCounter.SetText(fmt.Sprintf("%dx", counter.newCombo))
 }
 
 func (counter *ComboCounter) Reset() {
-	if counter.newCombo > 20 && counter.combobreak != nil && !counter.audioDisabled {
-		counter.combobreak.Play()
+	if counter.newCombo > 20 && counter.comboBreak != nil && !counter.audioDisabled {
+		counter.comboBreak.Play()
 	}
 
 	counter.newCombo = 0
+
+	counter.popCounter.SetText(fmt.Sprintf("%dx", counter.newCombo))
 }
 
 func (counter *ComboCounter) GetCombo() int {
@@ -88,16 +98,17 @@ func (counter *ComboCounter) DisableAudioSubmission(b bool) {
 }
 
 func (counter *ComboCounter) animate(time float64) {
-	counter.newComboScale.Reset()
-	counter.newComboScale.AddEventSEase(time, time+50, 1.28, 1.4, easing.InQuad)
-	counter.newComboScale.AddEventSEase(time+50, time+100, 1.4, 1.28, easing.OutQuad)
+	counter.mainCounter.ClearTransformationsOfType(animation.Scale)
+	counter.mainCounter.AddTransform(animation.NewSingleTransform(animation.Scale, easing.InQuad, time, time+50, 1, 1.094))
+	counter.mainCounter.AddTransform(animation.NewSingleTransform(animation.Scale, easing.OutQuad, time+50, time+100, 1.094, 1))
 }
 
 func (counter *ComboCounter) Update(time float64) {
-
 	counter.delta += time - counter.time
+
 	if counter.delta >= 16.6667 {
 		counter.delta -= 16.6667
+
 		if counter.combo > counter.newCombo && counter.newCombo == 0 {
 			counter.combo--
 		}
@@ -109,20 +120,24 @@ func (counter *ComboCounter) Update(time float64) {
 		counter.nextEnd = math.MaxInt64
 	}
 
+	counter.mainCounter.SetText(fmt.Sprintf("%dx", counter.combo))
+
 	counter.time = time
 
-	counter.newComboScale.Update(time)
-	counter.newComboScaleB.Update(time)
-	counter.newComboFadeB.Update(time)
+	counter.mainCounter.Update(time)
+	counter.popCounter.Update(time)
+
 	counter.comboSlide.Update(time)
 }
 
 func (counter *ComboCounter) SlideOut() {
-	counter.comboSlide.AddEvent(counter.time, counter.time+500, -1)
+	counter.comboSlide.AddEventEase(counter.time, counter.time+1000, -130, easing.InQuad)
+	counter.mainCounter.AddTransform(animation.NewSingleTransform(animation.Fade, easing.Linear, counter.time, counter.time+1000, counter.mainCounter.GetAlpha(), 0.0))
 }
 
 func (counter *ComboCounter) SlideIn() {
-	counter.comboSlide.AddEvent(counter.time, counter.time+500, 0)
+	counter.comboSlide.AddEventEase(counter.time, counter.time+1000, 0, easing.OutQuad)
+	counter.mainCounter.AddTransform(animation.NewSingleTransform(animation.Fade, easing.Linear, counter.time, counter.time+1000, counter.mainCounter.GetAlpha(), 1.0))
 }
 
 func (counter *ComboCounter) Draw(batch *batch.QuadBatch, alpha float64) {
@@ -132,31 +147,31 @@ func (counter *ComboCounter) Draw(batch *batch.QuadBatch, alpha float64) {
 		return
 	}
 
-	cmbSize := counter.comboFont.GetSize() * settings.Gameplay.ComboCounter.Scale
+	batch.ResetTransform()
+	batch.SetColor(1, 1, 1, comboAlpha)
 
 	slideAmount := counter.comboSlide.GetValue()
-
 	if settings.Gameplay.ComboCounter.XOffset > 0.01 {
 		slideAmount = 0
-		comboAlpha *= 1 + counter.comboSlide.GetValue()
 	}
 
-	posX := slideAmount*counter.comboFont.GetWidth(cmbSize*counter.newComboScale.GetValue(), fmt.Sprintf("%dx", counter.combo)) + 2.5
-	posY := counter.ScaledHeight - 12.8
+	xPos := settings.Gameplay.ComboCounter.XOffset + 3.2 + slideAmount
+	yPos := settings.Gameplay.ComboCounter.YOffset + counter.ScaledHeight - 12.8
+
+	batch.SetTranslation(vector.NewVec2d(xPos, yPos))
+
+	scl := settings.Gameplay.ComboCounter.Scale * 1.28
+
+	batch.SetScale(scl, scl)
+
 	origY := counter.comboFont.GetSize()*0.375 - 9
 
-	batch.ResetTransform()
-	batch.SetTranslation(vector.NewVec2d(settings.Gameplay.ComboCounter.XOffset, settings.Gameplay.ComboCounter.YOffset))
+	counter.popCounter.SetPosition(vector.NewVec2d(-3, origY).Scl(scl * counter.popCounter.GetScale().X))
+	counter.mainCounter.SetPosition(vector.NewVec2d(0, origY).Scl(scl * counter.mainCounter.GetScale().X))
 
-	batch.SetAdditive(true)
+	counter.popCounter.Draw(0, batch)
+	counter.mainCounter.Draw(0, batch)
 
-	batch.SetColor(1, 1, 1, counter.newComboFadeB.GetValue()*comboAlpha)
-	counter.comboFont.DrawOrigin(batch, posX-2.4*counter.newComboScaleB.GetValue()*settings.Gameplay.ComboCounter.Scale, posY+origY*counter.newComboScaleB.GetValue()*settings.Gameplay.ComboCounter.Scale, vector.BottomLeft, cmbSize*counter.newComboScaleB.GetValue(), false, fmt.Sprintf("%dx", counter.newCombo))
-
-	batch.SetAdditive(false)
-
-	batch.SetColor(1, 1, 1, comboAlpha)
-	counter.comboFont.DrawOrigin(batch, posX, posY+origY*counter.newComboScale.GetValue()*settings.Gameplay.ComboCounter.Scale, vector.BottomLeft, cmbSize*counter.newComboScale.GetValue(), false, fmt.Sprintf("%dx", counter.combo))
-
+	batch.SetColor(1, 1, 1, 1)
 	batch.ResetTransform()
 }
