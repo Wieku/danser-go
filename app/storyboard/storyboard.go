271578f7153f13b44a0099aff6f7c2e4aadc83be
Sebastian Krajewski <mail@wieku.me>
2022-06-29 02:29:13 +0200

    Always load "storyboard" samples, regardless of LoadStoryboards settings

--- app/storyboard/storyboard.go
+++ app/storyboard/storyboard.go
@@ -129,7 +129,18 @@ func NewStoryboard(beatMap *beatmap.BeatMap) *Storyboard {
 					}
 				}
 
-				if settings.Playfield.Background.LoadVideos && (strings.HasPrefix(line, "Video") || strings.HasPrefix(line, "1")) {
+				if strings.HasPrefix(line, "Sample") || strings.HasPrefix(line, "5") {
+					spl := strings.Split(line, ",")
+
+					startTime, _ := strconv.ParseFloat(spl[1], 64)
+					volume, _ := strconv.ParseFloat(spl[4], 64)
+
+					sbSprite := sprite.NewAudioSprite(storyboard.getSample(files2.FixName(spl[3])), startTime, volume/100)
+
+					storyboard.addSpriteToLayer(spl[2], sbSprite)
+
+					hasAudio = true
+				} else if settings.Playfield.Background.LoadVideos && (strings.HasPrefix(line, "Video") || strings.HasPrefix(line, "1")) {
 					spl := strings.Split(line, ",")
 
 					video := video2.NewVideo(filepath.Join(path, files2.FixName(spl[2])), -1, vector.NewVec2d(320, 240), vector.Centre)
@@ -155,18 +166,7 @@ func NewStoryboard(beatMap *beatmap.BeatMap) *Storyboard {
 
 					hasVideo = true
 				} else if settings.Playfield.Background.LoadStoryboards {
-					if strings.HasPrefix(line, "Sample") || strings.HasPrefix(line, "5") {
-						spl := strings.Split(line, ",")
-
-						startTime, _ := strconv.ParseFloat(spl[1], 64)
-						volume, _ := strconv.ParseFloat(spl[4], 64)
-
-						sbSprite := sprite.NewAudioSprite(storyboard.getSample(files2.FixName(spl[3])), startTime, volume/100)
-
-						storyboard.addSpriteToLayer(spl[2], sbSprite)
-
-						hasAudio = true
-					} else if strings.HasPrefix(line, "Sprite") || strings.HasPrefix(line, "4") || strings.HasPrefix(line, "Animation") || strings.HasPrefix(line, "6") {
+					if strings.HasPrefix(line, "Sprite") || strings.HasPrefix(line, "4") || strings.HasPrefix(line, "Animation") || strings.HasPrefix(line, "6") {
 						if currentSprite != "" {
 							storyboard.loadSprite(currentSprite, commands)
 						}
