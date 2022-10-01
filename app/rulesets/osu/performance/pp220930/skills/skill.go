3fa25d054af0b11751e548721a3202adde760922
Sebastian Krajewski <mail@wieku.me>
2022-10-01 16:34:45 +0200

    Port difficulty

--- app/rulesets/osu/performance/pp220930/skills/skill.go
+++ app/rulesets/osu/performance/pp220930/skills/skill.go
@@ -105,22 +105,6 @@ func (skill *Skill) startNewSectionFrom(end float64, current *preprocessing.Diff
 	skill.currentSectionPeak = skill.CalculateInitialStrain(end, current)
 }
 
-func (skill *Skill) CountDifficultStrains(clockRate float64) float64 {
-	peaks := skill.GetCurrentStrainPeaks()
-
-	var topStrain, realtimeCount float64
-
-	for _, v := range peaks {
-		topStrain = math.Max(topStrain, v)
-	}
-
-	for _, v := range peaks {
-		realtimeCount += math.Pow(v/topStrain, 4)
-	}
-
-	return realtimeCount * clockRate
-}
-
 func reverseSortFloat64s(arr []float64) {
 	sort.Float64s(arr)
 
