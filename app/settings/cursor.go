131a06d4dae3f5ecffcbc65504cb7fb3a87efa12
Sebastian Krajewski <mail@wieku.me>
2023-08-12 21:10:51 +0200

    Default CursorRipples to false as a lot of people confuse them with HitLighting

--- app/settings/cursor.go
+++ app/settings/cursor.go
@@ -40,7 +40,7 @@ func initCursor() *cursor {
 		GlowEndScale:                0.4,
 		InnerLengthMult:             0.9,
 		AdditiveBlending:            true,
-		CursorRipples:               true,
+		CursorRipples:               false,
 		SmokeEnabled:                true,
 	}
 }
