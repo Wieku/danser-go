870aab03c2cde4bcbc9f810c215bdca3afb75602
Sebastian Krajewski <mail@wieku.me>
2023-08-12 22:55:59 +0200

    Remove Min/Max from mutils package

--- framework/math/mutils/utils.go
+++ framework/math/mutils/utils.go
@@ -20,22 +20,6 @@ func Abs[T constraints.Integer | constraints.Float](a T) T {
 	return a
 }
 
-func Min[T constraints.Integer | constraints.Float](a, b T) T {
-	if a < b {
-		return a
-	}
-
-	return b
-}
-
-func Max[T constraints.Integer | constraints.Float](a, b T) T {
-	if a > b {
-		return a
-	}
-
-	return b
-}
-
 func Clamp[T constraints.Integer | constraints.Float](v, minV, maxV T) T {
 	return min(maxV, max(minV, v))
 }
