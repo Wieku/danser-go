92a680223d49aab4de4909fc5025d60b2c7a96dd
Sebastian Krajewski <mail@wieku.me>
2024-10-29 00:38:58 +0100

    Remove ApiV1Key and api.txt migration bits

--- app/settings/credentials.go
+++ app/settings/credentials.go
@@ -10,7 +10,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 )
 
@@ -20,8 +19,6 @@ var Credentails = &credentials{
 }
 
 type credentials struct {
-	ApiV1Key string `skip:"true" showif:"ApiVersion=v1" label:"API V1 Key" long:"true" password:"true" tooltip:"Valid API V1 Key has to be provided to have access to global leaderboards\n\nDON'T SHARE IT WITH OTHERS!!!" hidePath:"true"`
-
 	ClientId     string
 	ClientSecret string `long:"true" password:"true"`
 
@@ -37,8 +34,6 @@ type credentials struct {
 var srcDataCred []byte
 
 func LoadCredentials() {
-	migrateKey()
-
 	if err := os.MkdirAll(env.ConfigDir(), 0755); err != nil {
 		panic(err)
 	}
@@ -92,32 +87,6 @@ func SaveCredentials(forceSave bool) {
 
 		if err = os.WriteFile(fPath, data, 0644); err != nil {
 			panic(err)
-		}
-	}
-}
-
-func migrateKey() {
-	_, err := os.Stat(filepath.Join(env.DataDir(), "api.txt"))
-	if err == nil {
-		err = os.Rename(filepath.Join(env.DataDir(), "api.txt"), filepath.Join(env.ConfigDir(), "api.txt"))
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	file, err := os.Open(filepath.Join(env.ConfigDir(), "api.txt"))
-	if err == nil {
-		var data []byte
-
-		data, err = io.ReadAll(files.NewUnicodeReader(file))
-
-		file.Close()
-
-		if err == nil {
-			Credentails.ApiV1Key = strings.TrimSpace(string(data))
-			SaveCredentials(false)
-
-			os.Remove(filepath.Join(env.ConfigDir(), "api.txt"))
 		}
 	}
 }
